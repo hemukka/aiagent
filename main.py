6694c4e0d145974f13571bda7aea6777d0129e48
hemukka <henri.specops@hotmail.com>
2025-06-29 20:05:06 +0300

    store prompts in a list and add --verbose tag

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    if len(sys.argv) != 2:
-        print('usage: python3 main.py "<prompt>"')
+    
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    if not args:
+        print("AI Code Assistant")
+        print('Usage: python main.py "<prompt>" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+    user_prompt = " ".join(args)
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+
+    if verbose:
+        print("User prompt: ", user_prompt)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=sys.argv[1]
+        contents=messages
     )
 
-    if response.usage_metadata is not None:
+    if verbose and response.usage_metadata is not None:
         print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
         print("Response tokens: ", response.usage_metadata.candidates_token_count)
     print("Response:")
